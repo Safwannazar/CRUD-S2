13ff4660ee7b0de53d98ce68db6747858cb4daa4
Safwannazar <safwannazar99@gmail.com>
2023-04-11 14:56:11 +0800

    login

--- CRUD.js
+++ CRUD.js
@@ -1,3 +1,26 @@
+let dbUsers = [
+  {
+      Username : "Safwan",
+      password : "safwan14",
+      name : "safwan",
+      email : "safwannazar99@gmail.com"
+  }
+  ,
+  {
+      Username : "Ali",
+      password : "1234",
+      name : "ALi",
+      email : "ali@gmail.com"
+  }
+  ,
+  {
+      Username : "abu",
+      password : "12345",
+      name : "Abut",
+      email : "abu88@gmail.com"
+  }
+]
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,10 +29,13 @@ app.use(express.json())
 
 app.post('/login', (req, res) => {
   console.log(req.body)
-  res.send('login')
+
+  let result = login(req.body.username,req.body.password)
+
+  res.send(result)
 })
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('A B C D F G H I J K L M N O P Q R S T U V W X Y Z')
 })
 
@@ -18,9 +44,57 @@ app.get('/bye', (req, res) => {
   })
 
 app.post('/register', (req, res) => {
-    res.send('Account created')
+
+    let result= register
+    (
+
+      req.body.username,
+      req.body.password,
+      req.body.name,
+      req.body.email
+
+    )
+
+    res.send(result)
   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function login(reqUsername,reqpassword)
+{
+    let matchuser= dbUsers.find(
+        x => x.Username == reqUsername)
+
+    if (!matchuser)return "User not found"
+    if(matchuser.password==reqpassword)
+{
+    return matchuser
+}
+else
+{
+    return "invalid password"
+}
+
+}
+
+function register(reqUsername, reqpassword, reqname,reqemail)
+{
+    dbUsers.push
+    (
+        {
+        Username: reqUsername,
+        password: reqpassword,
+        name:   reqname,
+        email : reqemail    
+        }
+    )
+}
+
+
+//try to log in
+console.log(login ("Safwan","safwan14"))
+
+register("alibaba","1234","alisepet","alisepet@gmail.com")
+console.log(login ("alibaba","1234"))
